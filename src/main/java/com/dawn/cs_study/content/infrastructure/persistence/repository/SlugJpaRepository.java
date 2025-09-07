456170e516bbd75aa8bdf40034699500b70ad2f2
masiljangajji <xmfpdlsj0508@gmail.com>
2025-09-08 00:09:23 +0900

    feat: SlugRepository 패키지 위치 변경 및 트랜잭션 어노테이션 추가

--- src/main/java/com/dawn/cs_study/content/infrastructure/persistence/repository/SlugJpaRepository.java
+++ src/main/java/com/dawn/cs_study/content/infrastructure/persistence/repository/SlugJpaRepository.java
@@ -1,4 +1,4 @@
-package com.dawn.cs_study.content.infrastructure.jpa;
+package com.dawn.cs_study.content.infrastructure.persistence.repository;
 
 import com.dawn.cs_study.content.domain.Slug;
 import org.springframework.data.jpa.repository.JpaRepository;
--- src/main/java/com/dawn/cs_study/content/infrastructure/persistence/repository/SlugRepositoryImpl.java
+++ src/main/java/com/dawn/cs_study/content/infrastructure/persistence/repository/SlugRepositoryImpl.java
@@ -1,19 +1,22 @@
-package com.dawn.cs_study.content.infrastructure.jpa;
+package com.dawn.cs_study.content.infrastructure.persistence.repository;
 
 import com.dawn.cs_study.content.application.port.out.SlugRepository;
 import com.dawn.cs_study.content.domain.Slug;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
+@Transactional
 @RequiredArgsConstructor
 public class SlugRepositoryImpl implements SlugRepository {
 
     private final SlugJpaRepository repository;
 
     @Override
+    @Transactional(readOnly = true)
     public Optional<Slug> findById(Long id) {
         return repository.findById(id);
     }
