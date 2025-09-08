ddaa9fb937cdefbd19087cc851bf69f9bfefcf37
masiljangajji <xmfpdlsj0508@gmail.com>
2025-09-08 17:38:33 +0900

    refactor: 패키지 구조 변경

--- src/main/java/com/dawn/cs_study/content/application/port/out/ContentReaderPort.java
+++
@@ -1,9 +0,0 @@
-package com.dawn.cs_study.content.application.port.out;
-
-public interface ContentReaderPort {
-
-    String readText(String key);
-
-    <T> T readJson(String key, Class<T> type);
-
-}
--- src/main/java/com/dawn/cs_study/content/application/port/out/MarkdownReaderPort.java
+++
@@ -1,7 +0,0 @@
-package com.dawn.cs_study.content.application.port.out;
-
-public interface MarkdownReaderPort {
-
-    String toHtml(String md);
-
-}
