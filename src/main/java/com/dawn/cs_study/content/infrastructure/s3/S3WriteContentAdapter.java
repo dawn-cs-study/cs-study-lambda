1027e083fd1632cedd85fb33bfc1236e853910d2
masiljangajji <xmfpdlsj0508@gmail.com>
2025-09-08 17:43:19 +0900

    feat: WritContent 추가

---
+++ src/main/java/com/dawn/cs_study/content/infrastructure/s3/S3WriteContentAdapter.java
@@ -0,0 +1,47 @@
+package com.dawn.cs_study.content.infrastructure.s3;
+
+import com.dawn.cs_study.content.application.port.out.WriteContentPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+
+@Component
+@RequiredArgsConstructor
+public class S3WriteContentAdapter implements WriteContentPort {
+
+    private final S3Client s3Client;
+
+    @Override
+    public void upload(String bucket, String key, byte[] content, String contentType) {
+        PutObjectRequest request = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .contentType(contentType)
+                .build();
+
+        s3Client.putObject(request, RequestBody.fromBytes(content));
+    }
+
+    @Override
+    public void delete(String bucket, String key) {
+        try {
+            DeleteObjectRequest request = DeleteObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(key)
+                    .build();
+            s3Client.deleteObject(request);
+        } catch (S3Exception e) {
+            if (e.statusCode() == 404) {
+                // 이미 없는 객체라면 무시 → 멱등성 보장
+                return;
+            }
+            throw e;
+        }
+    }
+
+
+}
