458b1f74bfd7af92b7315e8b5873824884c960f4
masiljangajji <xmfpdlsj0508@gmail.com>
2025-09-12 04:02:44 +0900

    refactor: 미사용 UseCase 삭제

--- lambda-md-sync/src/main/java/com/dawn/cs/study/lambda/md/application/ReadContentUseCase.java
+++ lambda-md-sync/src/main/java/com/dawn/cs/study/lambda/md/application/ReadContentUseCase.java
@@ -3,21 +3,25 @@ package com.dawn.cs.study.lambda.md.application;
 import com.dawn.cs.study.lambda.md.application.port.ReadContentPort;
 import com.dawn.cs.study.lambda.md.application.port.ReadMarkdownPort;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-public class ReadContentService {
+public class ReadContentUseCase {
 
     private final ReadContentPort readContentPort;
     private final ReadMarkdownPort readMarkdownPort;
 
     public String renderHtml(String documentName) {
+        log.info("renderHtml start {}", documentName);
         String md = readContentPort.readText(documentName);
         return readMarkdownPort.toHtml(md);
     }
 
     public <T> T readJson(String key, Class<T> type) {
+        log.info("readJson start {}", key);
         return readContentPort.readJson(key, type);
     }
 }
